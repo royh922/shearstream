9e8f0d6273d3407f2f2f0f1939664bcac58680d3
Jui-Teng Hsu <juitenghsu@umass.edu>
2025-11-06 17:55:06 -0500

    Changed plot script to parse arguments.

--- plot_gen.py
+++ plot_gen.py
@@ -1,5 +1,11 @@
 import yt
 import unyt
+import argparse
+
+parser = argparse.ArgumentParser(description="Plot a field from a dataset")
+parser.add_argument("prefix", type=str, help="Prefix for the dataset series")
+parser.add_argument("field", type=str, help="Field to plot")
+args = parser.parse_args()
 
 yt.enable_parallelism()
 
@@ -11,22 +17,27 @@ def temperature(field, data):
 
 # Load all of the DD*/output_* files into a DatasetSeries object
 # in this case it is a Time Series
-prefix = 'MHD_1000'
-ts = yt.load(f"./{prefix}/KH-MHD-*/KH-MHD-*.block_list")
+prefix = args.prefix
+field = args.field
+ts = yt.load(f"./{prefix}/KH-*/KH*.block_list")
 
 # Define an empty storage dictionary for collecting information
 # in parallel through processing
 storage = {}
 global_extrema = [float('inf'), float('-inf')]
-field="temperature"
 
 # Use piter() to iterate over the time series, one proc per dataset
 # and store the resulting information from each dataset in
 # the storage dictionary
 for sto, ds in ts.piter(storage=storage):
-    p = yt.SlicePlot(ds, "z", ('gas', f'{field}'))
-    p.set_zlim(field, zmin=1e4)
-    p.set_cmap(field, 'RdBu_r')
+    p = yt.ProjectionPlot(ds, "z", ('gas', f'{field}'), weight_field=('gas', 'density'))
+    # p.set_zlim(field, zmin=1e4)
+    p.set_cmap(field, 'RdBu')
+    if field == 'temperature':
+        p.set_cmap(field, 'RdBu_r')
+    # p.annotate_scale(corner='lower_left')
+    p.annotate_timestamp(time_unit="Myr",corner='upper_right', time_format='t = {time:.3f} {units}')
+    p.annotate_title(f"{field} Projection")
     p.save(name=f'{prefix}/{field}/')
     dd = ds.all_data()
     sto.result = dd.quantities.extrema(("gas", f"{field}"))
@@ -43,4 +54,4 @@ if yt.is_root():
 #    p = yt.SlicePlot(ds, "z", ('gas', f'{field}'))
 #    p.set_cmap(field, 'RdBu')
 #    p.set_zlim(field, zmin=global_extrema[0], zmax=global_extrema[1])
-#    p.save(name=f'{field}/')   
+#    p.save(name=f'{field}/')  
